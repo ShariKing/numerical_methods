f7fde8ac4004a81eb7dfee3a04d10b2b5f1b3dce
Shari King <king_shari@hotmail.com>
2011-11-21 21:46:42 -0500

    corrected error in k1

--- project_2/rkg.m
+++ project_2/rkg.m
@@ -5,7 +5,7 @@ deltax=delx;
 f = func;
 
 k1 = f(xi,yi);
-k2 = f(xi + deltax/2, yi+deltax/2);
+k2 = f(xi + deltax/2, yi+ ((deltax/2)*k1));
 k3 = f(xi + deltax/2, yi + ((-1/2)+(1/sqrt(2)))*deltax*k1 + (1-(1/sqrt(2)))*deltax *k2);
 k4 = f(xi + deltax, yi - (1/sqrt(2))*deltax*k2 + (1+(1/sqrt(2)))*deltax*k3);
 
